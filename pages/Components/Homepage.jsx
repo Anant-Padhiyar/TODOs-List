4319a3b8a1a0e378afa5c4dc392cb000dc93646f
Anant Padhiyar <133571116+Anant-Padhiyar@users.noreply.github.com>
2024-03-14 01:41:44 +0530

    Update Homepage.jsx

--- pages/Components/Homepage.jsx
+++ pages/Components/Homepage.jsx
@@ -141,16 +141,21 @@ style={{ backgroundColor: mode === 'light' ? 'white' : 'black', color: mode === 
 <div className={`modal fade ${mode === 'light' ? 'light-mode' : 'dark-mode'}`} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 <div className="modal-dialog">
 <div className="modal-content">
+
 <div className="modal-header">
 <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 <div className="modal-body">
+
+{/*
 <div className="mb-3">
 <label htmlFor="EditT" className="form-label">TODO Title</label>
 <input type="text" className="form-control" id="EditT" aria-describedby="emailHelp" value={Title} onChange={handleTitleChange}
 style={{ backgroundColor: mode === 'light' ? 'white' : 'black', color: mode === 'light' ? 'black' : 'white' }}/>
 </div>
+/*}
+
 <div className="mb-3">
 <label htmlFor="EditD" className="form-label">TODO Description</label>
 <textarea className="form-control" id="EditD" rows="3" value={Desc} onChange={handleDescChange}
